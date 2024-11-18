0d1f9f41f3f2bb01ab459b9cbe57762ec661221e
James Gore <james.gore@maine.edu>
2024-11-18 14:06:21 -0500

    fix file casing issue

--- WgaFetcher.py
+++ WgaFetcher.py
@@ -1,14 +1,55 @@
-import requests
-import urllib.request
-import os
-import re
 from time import sleep
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from WgaMetadata import WgaMetadata
+import requests
+import urllib.request
+import os
+import re
 
 IMAGE_DIR='images/'
 METADATA_PATH='metadata.json'
+
+def rename_uncased_duplicates(directory,rename=True)-> dict:
+    """
+    Returns a dictionary of renamed images, the key is the renamed image and the value is the original name
+    If rename is set to true the file will be renamed, if set to false nothing will change
+    """
+    renames={}
+    images=os.listdir(directory)
+    lower_case_images=[image.lower() for image in images]
+    # A dictionary used to keep track of multiple uncased duplicates
+    duplicates_dict={}
+    for i in range(len(lower_case_images)):
+        image=lower_case_images.pop(0)
+        # pop the same image from images so that they are aligned
+        images.pop(0)
+        # This if statement checks that after popping the head of the list its still in the list
+        if image in lower_case_images:
+            dupe_idx=0
+            for idx in range(len(lower_case_images)):
+                if lower_case_images[idx]==image:
+                    dupe_idx=idx
+            image_path=os.path.join(directory,images[dupe_idx])
+            renamed_image_path=os.path.join(directory,images[dupe_idx][:-4]+f"(d{duplicates_dict[image] if image in duplicates_dict else ''}).jpg")
+            print(f"uncased duplicate found for {image_path}")
+            #Rename the file with a (d) for duplicate which makes them different
+            if rename:
+                os.rename(
+                    image_path,
+                    # Remove .jpg extension and add uncased duplicate identifier
+                    renamed_image_path,                
+                )
+            # Update duplicate dict
+            if image not in duplicates_dict:
+                duplicates_dict[image]=1
+            else:
+                duplicates_dict[image]+=1
+            
+            renames[renamed_image_path]=image_path
+                
+    return renames
+
 
 class WgaFetcher():
     def __init__(self) -> None:
--- rename_images.py
+++ rename_images.py
@@ -3,30 +3,38 @@
 # On linux this issue is is non-existent as the difference in casing makes them unique files
 # If the operating system does not take case into account these will be considered the same file
 # To fix this issue we rename the duplicates
+from WgaFetcher import rename_uncased_duplicates
+import json
+import sys
 import os
 
+metadata_path=sys.argv[1]
 
-IMG_DIR="../../images"
-for directory in os.listdir(IMG_DIR):
-    images=os.listdir(os.path.join(IMG_DIR,directory))
-    lower_case_images=[image.lower() for image in images]
-    # A dictionary used to keep track of multiple uncased duplicates
-    duplicates_dict={}
-    for i in range(len(lower_case_images)):
-        image=lower_case_images.pop(0)
-        # This if statement checks that after popping the head of the list its still in the list
-        if image in lower_case_images:
-            dupe_idx=lower_case_images.index(image)
-            image_path=os.path.join(IMG_DIR,directory,images[dupe_idx])
-            print(f"uncased duplicate found at {image_path}")
-            #Rename the file with a (d) for duplicate which makes them different
-            os.rename(
-                image_path,
-                # Remove .jpg extension and add uncased duplicate identifier
-                image_path[:-4]+f" (d{duplicates_dict[image] if image in duplicates_dict else ''}).jpg"                
-            )
-            # Update duplicate dict
-            if image not in duplicates_dict:
-                duplicates_dict[image]=1
-            else:
-                duplicates_dict[image]+=1
+FP="file_path"
+
+with open(metadata_path,encoding="utf-8") as metadata_file:
+    metadata=json.load(metadata_file)
+
+base_dir_grouping={}
+
+for key in metadata:
+    image_dir=os.path.dirname(metadata[key][FP])
+    if image_dir not in base_dir_grouping:
+        base_dir_grouping[image_dir]=[key]
+    else:
+        base_dir_grouping[image_dir]+=key
+    
+modified_files=[]
+for directory in base_dir_grouping:
+    renamed=rename_uncased_duplicates(directory,rename=True)
+    if renamed:
+        modified_files.append(renamed)
+        
+for renames in modified_files:
+    for key,value in renames.items():
+        for id in metadata:
+            if metadata[id][FP]==value:
+                metadata[id][FP]=key
+        
+with open(metadata_path,"w",encoding="utf-8") as metadata_file:
+    metadata_file.write(json.dumps(metadata,indent=4,ensure_ascii=False))
